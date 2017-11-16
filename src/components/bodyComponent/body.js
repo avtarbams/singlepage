0239f82ace758dae194b2c7baca2b7c3ee242366
Avtar Singh Bamra <31774649+avtarbams@users.noreply.github.com>
2017-11-16 15:42:55 -0500

    Initial commit

---
+++ src/components/bodyComponent/body.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <body>
+     <div className="container-fluid">
+     <div className="row">
+         <div className="col-md-10">
+            <p>
+              Hi, Welcome to my blog. This blog is created only for educational purpose and serve the purpose to demonstate the use of ReactJS.
+            </p>
+            <div className="blogpost">
+            <div class="container">
+<div class="row">
+    <div class="col-md-1"></div>
+	<div class="col-md-8">
+		<div id="postlist">
+			<div class="panel">
+                <div class="panel-heading">
+                        <div class="row">
+                            <div class="column-fluid">
+                                <h1>IMAGE-TO-TEXT-TO-VOICE CONVERSION</h1>
+                            </div>
+                            <div class="col-sm-2">
+                                <h5 class="pull-down">
+                                <small><em>11/15/2017</em></small>
+                                </h5>
+                            </div>
+                        </div>
+                  
+                </div>
+                
+            <div class="panel-body">
+            The real world environment comprises of numerous objects, articles and entities of various shapes, sizes and orientations. For example, in a classroom, there are desks, markers, boards. A person may sometimes need assistance in locating an object that may be hidden from view or whose location is forgotten. In that case, using an object locator comes in handy.
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            <p></p>
+            <div className="blogpost">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-1"></div>
+              <div class="col-md-8">
+                <div id="postlist">
+                  <div class="panel">
+                            <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="column-fluid">
+                                            <h1 id='november'>IMAGE MANIPULATION WORKSHOP Using Photoshop</h1>
+                                        </div>
+                                        <div class="col-sm-2">
+                  
+                                            <h5 class="pull-down">
+                                            <small><em>11/10/2017</em></small>
+                                            </h5>
+                                        </div>
+                                    </div>
+                              
+                            </div>
+                            
+                        <div class="panel-body">
+                        I always believe that sharing is caring. While pursuing my Bachelor's, I have conducted a workshop on Image manipulation. The workshop was held in association with the Computer Society of India with over 200 students participating from several engineering backgrounds. More details can be viewed at www.csi.ppit.ac.in by clicking on the Image Manipulation Workshop.
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+            <p></p>
+            <div className="blogpost">
+            <div class="container">
+            <div class="row">
+                <div class="col-md-1"></div>
+              <div class="col-md-8">
+                <div id="postlist">
+                  <div class="panel">
+                            <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="column-fluid">
+                                            <h1 id='june'>SWACHH BHARAT (CLEAN INDIA) MOVIE</h1>
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <h5 class="pull-down">
+                                            <small><em>06/15/2017</em></small>
+                                            </h5>
+                                        </div>
+                                    </div>
+                              
+                            </div>
+                            
+                        <div class="panel-body">
+                        Promoting the Clean India Drive initiated by the Prime Minister of India, Mr. Narendra Modi, we visited a Mumbai Municipal School and spread the awareness regarding cleanliness and its importance to the society. I covered the event in the form of a video using my smartphone with the phone's microphone acting as an audio recording source.
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+         </div>
+         <div className="col-md-2">
+         <div id="datechronology">
+            <ul>
+              <li><a href="#november">November 2017</a></li>
+              <li>October 2017</li>
+              <li>September 2017</li>
+              <li>August 2017</li>
+              <li>July 2017</li>
+              <li><a href="#june">June 2017</a></li>
+              <li>May 2017</li>
+              <li>April 2017</li>
+              <li>March 2017</li>
+              </ul>
+         </div>
+
+         Follow me on:<br />
+             <ul>
+               <li>
+               <a class="btn btn-social-icon btn-twitter" href="http://www.twitter.com/avtarbams" target="_blank" >
+    <span class="fa fa-twitter"></span>
+  </a>
+               </li>
+               <li>
+               <a class="btn btn-social-icon btn-facebook" href="http://www.facebook.com/avtarbams" target="_blank" >
+    <span class="fa fa-facebook"></span>
+  </a>
+               </li>
+               <li>
+               <a class="btn btn-social-icon btn-instagram" href="http://www.instagram.com/avtarbams" target="_blank" >
+    <span class="fa fa-instagram"></span>
+  </a>
+               </li>
+               <li>
+               <a class="btn btn-social-icon btn-github" href="https://github.com/avtarbams/" target="_blank" >
+    <span class="fa fa-github"></span>
+  </a>
+               </li>
+               </ul>
+
+         </div>
+     </div>
+ </div>
+</body>
+
+    );
+  }
+}
+
+export default Body;
---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+     <header>
+       <div className="container-fluid">
+       <img src= "http://avtarbams.com/images/avtar.png" alt="" height="75"/>&nbsp; Welcome to Bams' Blog
+       </div>
+
+         </header>
+
+    );
+  }
+}
+
+export default Header;
